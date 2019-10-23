4256ea6413b5acad96249cbf0259ac90f3f64ba6
Sergio Ramos Santonja <sergioramos55@outlook.es>
2019-10-23 12:38:25 +0200

    Continuar.

--- app/src/main/java/com/example/propuestodialogo/MainActivity.java
+++ app/src/main/java/com/example/propuestodialogo/MainActivity.java
@@ -1,14 +1,33 @@
 package com.example.propuestodialogo;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button botonDialogo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        botonDialogo = (Button) findViewById(R.id.boton_dialogo);
+        botonDialogo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+
+                LayoutInflater inflater = MainActivity.this.getLayoutInflater();
+
+                builder.setView(inflater.inflate(R.layout.dialogo,null));
+
+                builder.show();
+            }
+        });
     }
 }
